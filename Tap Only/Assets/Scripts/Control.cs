f70c758a91d32c547f3f867b9fd8e28122725754
SardineFish <Sardine@live.cn>
2017-11-05 23:40:23 +0800

    Complete first demo

---
+++ Tap Only/Assets/Scripts/Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+    public Vector2 Force;
+    public GameObject CameraObject;
+    new Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        if (CameraObject)
+            camera = CameraObject.GetComponent<Camera>();
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+            Ray2D ray = new Ray2D(new Vector2(pos.x, pos.y), Vector2.zero);
+            if (Physics2D.Raycast(ray.origin, ray.direction))
+            {
+                GetComponent<Rigidbody2D>().AddForceAtPosition(Force, pos,ForceMode2D.Impulse);
+            }
+            var x = 0;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
---
+++ Tap Only/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, transform.position.z);
+    }
+}
